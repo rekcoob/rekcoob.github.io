cbd4eb3748c069120ea0ab74a586be9da9107342
b0ock3r <57836351+rekcoob@users.noreply.github.com>
2024-10-24 19:35:39 +0200

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(App)
+  .use(VueGtag, {
+    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID }
+  })
+  .mount('#app')
