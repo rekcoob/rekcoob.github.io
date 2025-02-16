19b8f91a34dc8ab5a233269d2cad7e7e76e85f39
b0ock3r <57836351+rekcoob@users.noreply.github.com>
2025-02-16 16:08:20 +0100

    ci: add EmailJs variables to Actions workflow

--- src/components/sections/ContactForm.vue
+++ src/components/sections/ContactForm.vue
@@ -6,9 +6,9 @@ const form = ref(null)
 const responseMessage = ref('')
 
 // Environment variables
-const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
-const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
-const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID ?? ''
+const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID ?? ''
+const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY ?? ''
 
 const sendEmail = async () => {
   try {
