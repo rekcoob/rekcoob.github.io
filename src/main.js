53917258ac47550ceda811826f6c3a59417292ce
b0ock3r <57836351+rekcoob@users.noreply.github.com>
2025-03-07 11:28:17 +0100

    chore: migrate tracking from vue-gtag to vue-gtag-next for Vue3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueGtag from 'vue-gtag'
-
-// createApp(App).mount('#app')
+// import VueGtag from 'vue-gtag'
+import VueGtag from 'vue-gtag-next'
 
 createApp(App)
   .use(VueGtag, {
-    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
+    property: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
   })
   .mount('#app')
+
+// createApp(App).mount('#app')
